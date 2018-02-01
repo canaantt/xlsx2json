c1ebf141fe61b612e656883935a5f8d8b0f9c44f
Jenny Zhang <canaantt@gmail.com>
2018-02-01 11:35:17 -0800

    .

--- xlsx-json.js
+++ xlsx-json.js
@@ -1,19 +1,7 @@
-xls_to_json = require("xls-to-json");
-xls_to_json({
-    input: "demo.xlsx",  // input xls 
-    output: "output.json", // output json 
-    sheet: "SAMPLE"  // specific sheetname 
-    }, function(err, result) {
-    if(err) {
-        console.error(err);
-    } else {
-        console.log(result);
-    }
-});
 const jsonfile = require("jsonfile");
 const _ = require("underscore");
 const asyncLoop = require('node-async-loop');
 const XLSX =require("xlsx");
 const fs = require('fs');
-
-sheetjs
+_.uniq(Object.keys(workbook.Sheets['PATIENTEVENT-CHEMO']).map(k=>workbook.Sheets['PATIENTEVENT-CHEMO'][k].t));
+Object.keys(workbook.Sheets['PATIENTEVENT-CHEMO']).filter(k=>workbook.Sheets['PATIENTEVENT-CHEMO'][k].t == 'undefined');
